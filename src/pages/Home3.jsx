7c6062d265945e8d8abae9f4590419dba4983967
Ashique <ansariazad7864@gmail.com>
2024-07-30 20:40:15 +0530

    ...

--- src/pages/Home3.jsx
+++ src/pages/Home3.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import DynamicButton from "../components/common/DynamicButton";
+import { funFact2 } from "../data/fun-fact-data.js";
 
 const Home3 = () => {
   return (
-    <div>Home3</div>
-  )
-}
+    <div>
+      <div className="pt-20 pr-64 hero-section-3 pl-80">
+        <div className="font-popfont-poppins">
+          <h1 className="font-bold text-56px">
+            Manage Your <br /> Business{" "}
+            <span className="text-state-success">Finance</span>
+            <br /> Easily
+          </h1>
+          <p className="pl-0 pr-96">
+            An enim nullam tempor sapien gravida donec enim ipsum porta justo
+            congue magna at pretium purus pretium ligula.
+          </p>
+        </div>
+        <div className="pt-10">
+          <DynamicButton
+            text="Start 14 Days Trial"
+            className={`w-44 font-poppins text-0.8rem h-12 leading-10`}
+          />
+        </div>
+      </div>
+      <div>
+        <div>
+          {funFact2.map((item) => (
+            <div key={funFact2.id}>{funFact2.count}</div>
+          ))}
+          <h1>{funFact2.count}</h1>
+          <p></p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home3
+export default Home3;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontSize: {
+      '56px': '56px',
+      '0.8rem': '0.8rem',
+    },
     fontFamily: {
       
       poppins: ["Poppins", "sans-serif"],
