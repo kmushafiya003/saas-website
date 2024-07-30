4af899c199d92fdc35d68422c104692b17f77ae2
Ashique <ansariazad7864@gmail.com>
2024-07-30 14:52:11 +0530

    ...

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import DynamicButton from './components/common/DynamicButton';
 
 function App() {
   return (
-    <div>
-    
+    <div className="flex flex-col items-center justify-center h-screen space-y-4">
+      <DynamicButton text='get started'/>
+      <DynamicButton text='click me' className={`text-primary-crocus-purple bg-primary-fuel-yellow`}/>
+      <DynamicButton text='read more' className={`text-black bg-secondary-soft-blue`}/>
     </div>
   );
 }
