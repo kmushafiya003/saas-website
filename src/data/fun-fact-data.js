a31e96658d641a2f64c2a5ba81ea7e3c4360bd2b
Mushafiya <kmushafiya003@gmail.com>
2024-07-30 12:05:38 +0530

    Added Fun-fact Data

---
+++ src/data/fun-fact-data.js
@@ -0,0 +1,28 @@
+import Icon1 from '../images/Icon1.svg'
+import Icon2 from '../images/Icon2.svg'
+import Icon3 from '../images/Icon3.svg'
+
+
+export const funFact1 = [
+    {
+        id:1,
+        image: Icon1,
+        count: "15k",
+        text: "Active user"
+
+    },
+    {
+        id:2,
+        image: Icon2,
+        count: "30k",
+        text: "Total Download"
+
+    },
+    {
+        id:3,
+        image: Icon3,
+        count: "10k",
+        text: "Customer"
+
+    },
+]
