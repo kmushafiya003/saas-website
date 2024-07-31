360ec5b9e7a4ba117dda3dd8d4b41c1e24073d05
akki9689 <akashpuri259@gmail.com>
2024-07-31 20:26:52 +0530

    final touch

--- src/components/common/Navbar2.jsx
+++ src/components/common/Navbar2.jsx
@@ -2,25 +2,21 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { NavLinks2 } from '../../data/navlinks';
 import logo1 from '../../images/Logo (1).svg';
-import logo2 from '../../images/Logo (2).svg'
+import logo2 from '../../images/Logo (2).svg';
 import { useLocation } from 'react-router-dom';
 
 const Navbar2 = () => {
-
     const location = useLocation();
+    const [logo, setLogo] = useState(1);
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
-    const [logo , setLogo] = useState(1);
-
-    useEffect(()=>{
-        if(location.pathname === "/home2"){
+    useEffect(() => {
+        if (location.pathname === "/home2") {
             setLogo(1);
-        }else{
+        } else {
             setLogo(2);
         }
-    }, [location.pathname])
-
-
-    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+    }, [location.pathname]);
 
     const handleMobileMenuToggle = () => {
         setIsMobileMenuOpen(!isMobileMenuOpen);
@@ -28,11 +24,11 @@ const Navbar2 = () => {
 
     return (
         <div className="container xl:px-4 px-1">
-            <nav className="  mmd: bg-white p-6 flex items-center gap-8 justify-center flex-wrap">
-                <div className="flex items-center justify-between w-full relative md:w-auto ">
-                    <img src={logo === 1 ? logo1 : logo === 2 ? logo2 : ""} alt="Logo" className="h-8 w-auto" />
+            <nav className="bg-white p-6 flex items-center justify-between flex-wrap">
+                <div className="flex items-center justify-between w-full relative md:w-auto">
+                    <img src={logo === 1 ? logo1 : logo2} alt="Logo" className="h-8 w-auto" />
                     <button
-                        className=" text-secondary-selective-yellow md:hidden"
+                        className="text-secondary-selective-yellow md:hidden"
                         onClick={handleMobileMenuToggle}
                     >
                         <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -41,7 +37,7 @@ const Navbar2 = () => {
                     </button>
                 </div>
 
-                <div className={`relative w-full md:flex  md:items-center md:w-auto ${isMobileMenuOpen ? 'block' : 'hidden'} ml-8`}>
+                <div className={`relative w-full md:flex md:items-center md:w-auto ${isMobileMenuOpen ? 'block' : 'hidden'} ml-8`}>
                     <ul className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 font-poppins items-center justify-center md:justify-start">
                         {NavLinks2.map((navItem) => (
                             <li key={navItem.id} className="text-black">
@@ -53,22 +49,12 @@ const Navbar2 = () => {
                     </ul>
 
                     <div className="flex flex-col md:flex-row md:space-x-6 mt-4 md:mt-0 items-center justify-center md:justify-start ml-8">
-                        <Link to="/login" className={` px-8 py-4 hover:text-black mb-2 md:mb-0
-                        ${
-                            logo === 1 ? "text-custom-orange cursor-pointer " : "text-secondary-light-turquoise"
-                        }
-                            `}>
+                        <Link to="/login" className={`px-8 py-4 hover:text-black mb-2 md:mb-0 ${logo === 1 ? "text-custom-orange cursor-pointer" : "text-secondary-light-turquoise"}`}>
                             Login
                         </Link>
                         <Link
                             to="/sign-up"
-                            className={`text-white  px-8 py-4 rounded-full  hover:text-white focus:outline-none focus:ring-1 focus:ring-selective-yellow
-
-                                ${
-                                    logo === 1 ? "bg-custom-orange hover:bg-orange-600  hover:bg-secondary-selective-yellow cursor-pointer" : "bg-secondary-light-turquoise hover:bg-secondary-light-turquoise "
-                                }
-                                
-                                `}
+                            className={`text-white px-8 py-4 rounded-full hover:text-white focus:outline-none focus:ring-1 focus:ring-selective-yellow ${logo === 1 ? "bg-custom-orange hover:bg-orange-600" : "bg-secondary-light-turquoise"}`}
                         >
                             Sign Up
                         </Link>
--- src/pages/Home2.jsx
+++ src/pages/Home2.jsx
@@ -6,88 +6,82 @@ const Home2 = () => {
   // Background image path
   const bgImage = '/Home2-dbg.svg';
   const whiteCornerSVG = '/home2-whitecorner.svg';
-  const svgTopRight= '/Alarm_Clock.svg'; // Replace with the actual path
+  const svgTopRight = '/Alarm_Clock.svg'; // Replace with the actual path
   const svgBottomLeft = '/rocket.svg';
 
   return (
-    <div className="relative min-h-screen text-custom-blend">
-
-<div className="absolute inset-0 bg-[#FFF8F2] opacity-6 z-15"></div>
-      <Navbar2/>
-
-
-
-
-      {/* Background Image */}
-      <div
-
-      >
-        {/* Color Overlay */}
-        
-      </div>
-
-      {/* Navbar */}
-      <header className="bg-gray-800 text-black shadow-md sticky top-0 z-20 bg-opacity-70">
-        
-      </header>
-
-      {/* Main Section */}
-      <section className="relative h-screen flex flex-col items-center justify-center bg-center bg-no-repeat xsm:w-[] " style={{ backgroundImage: `url(${bgImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }} >
-
-         {/* Text Div */}
-         <div className="relative z-20  px-5 pt-[180px] pb-[30px]    xsm:text-center md:px-12  md:py-12  lg:px-12 lg:py-16 max-w-screen-xl mx-auto mt-20 text-left  ">
-    {/* Heading */}
-    <h1 className="text-3xl dxs:text-3xl xxs:text-4xl xsm:text-5xl  xs:text-6xl lg:text-6xl  font-bold leading-tight text-black font-poppins  sm:mt-10 md: lg:">
-      <span className="block">A collaborative time</span>
-      <span className="block">Tracking that you Need</span>
-    </h1>
-    {/* Paragraph */}
-    <p className="text-xm mt-4 dxs:text-sm  xxs:text-base  xsm:text-lg   xs:text-xl  mb-6   lg:leading-relaxed max-w-lg mx-auto">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quia aliquam architecto beatae quo quibusdam.
-    </p>
-    {/* Button */}
-    <button className=" px-10 py-5 bg-custom-orange hover:bg-primary-fuel-yellow text-white font-semibold rounded-full shadow-md hover:bg-orange-600 transition-colors">
-      Start 14 Days Trial
-    </button>
-  </div>
-
-
-        {/* Fun Facts Section */}
-        <div className="relative z-30 flex flex-col flex-nowrap items-center justify-center w-full mt-auto pb-12  ">
-          {/* White SVG */}
-          <img src={whiteCornerSVG} alt="White Corner SVG" className="absolute inset-0 z-20" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
-
-          {/* Fun Facts */}
-
-          <div className="relative z-30 gap-2 flex flex-nowrap flex-col xsm:flex-row mr-[10%] mt-[5%]">
-      {funFact1.map((item, index) => (
-        <div
-          key={item.id}
-          className={` dxs:${index === funFact1.length - 1 ? 'border-none' : ' bg-white border-b-[1px]  border-gray-100 '}  xxs:border-none p-4 h-40 w-52  sm:p-10 md:p-10 lg:p-8 flex flex-col items-center `}
-        >
-          <div className="text-2xl mt-6 sm:text-xl md:text-2xl lg:text-3xl mt-2 font-semibold text-black mb-2">
-            {item.count}
-            {item.text === "Active user" && '+'}
-          </div>
-          <div className="text-xs sm:text-sm md:text-base lg:text-lg text-gray-700">
-            {item.text}
-          </div>
-          
-        </div>
-      ))}
+    
+    <div className="relative min-h-screen bg-[#ffffff] ">
+    <div className='min-w-full'>
+  <Navbar2/>
     </div>
 
-
+      {/* Main Section */}
+      <section
+        className="relative h-screen flex flex-col items-center justify-center bg-center bg-no-repeat"
+        style={{
+          backgroundImage: `url(${bgImage})`,
+          backgroundSize: 'cover', // Ensure the image is not scaled
+          backgroundPosition: 'center', // Center the image
+          backgroundRepeat: 'no-repeat', // Do not repeat the image
+          width: '100vw', // Full viewport width
+          height: '100vh', // Full viewport height
+        }}
+      >
+        {/* Text Div */}
+        <div className="relative z-20 mt-[300px] dxs:mt-[200px] p-[20px] xxs:mt-[190px] xsm:mt-[80px] text-center xs:mt-[40px] sm: md: lg: ">
+          {/* Heading */}
+          <h1 className="text-3xl dxs:text-3xl xxs:text-4xl xsm:text-5xl xs:text-6xl lg:text-6xl font-bold leading-tight text-black font-poppins sm:mt-10 md: lg:">
+            <span className="block">A collaborative time</span>
+            <span className="block">Tracking that you Need</span>
+          </h1>
+          {/* Paragraph */}
+          <p className="text-xm mt-4 dxs:text-sm xxs:text-base xsm:text-lg xs:text-xl mb-6 lg:leading-relaxed max-w-lg mx-auto">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quia aliquam architecto beatae quo quibusdam.
+          </p>
+          {/* Button */}
+          <button className="px-10 py-5 bg-custom-orange hover:bg-primary-fuel-yellow text-white font-semibold rounded-full shadow-md hover:bg-orange-600 transition-colors">
+            Start 14 Days Trial
+          </button>
         </div>
-        <div className="absolute inset-0 ">
-            
-            <img src={svgTopRight} alt="Top Right SVG" className="block xsm:hidden absolute top-0 right-12 w-14 h-14" />
-            <img src={svgBottomLeft} alt="Bottom Left SVG" className= "block xsm:hidden z-50 absolute bottom-[-200px] left-0 w-16 h-16"/>
-            
+
+        {/* Fun Facts Section */}
+        <div className="relative z-30 flex flex-col flex-nowrap items-center justify-center w-full mt-auto pb-12">
+          {/* White SVG */}
+          <img
+            src={whiteCornerSVG}
+            alt="White Corner SVG"
+            className="absolute inset-0 z-20"
+            style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+          />
+
+          {/* Fun Facts */}
+          <div className="relative z-30 gap-2 flex flex-nowrap flex-col xsm:flex-row mr-[10%] mt-[5%]">
+            {funFact1.map((item, index) => (
+              <div
+                key={item.id}
+                className={`dxs:${index === funFact1.length - 1 ? 'border-none' : 'bg-white border-b-[1px] border-gray-100'} xxs:border-none p-4 h-40 w-52 sm:p-10 md:p-10 lg:p-8 flex flex-col items-center`}
+              >
+                <div className="text-2xl  sm:text-xl md:text-2xl lg:text-3xl mt-8 font-semibold text-black mb-2">
+                  {item.count}
+                  {item.text === "Active user" && '+'}
+                </div>
+                <div className="text-xs sm:text-sm md:text-base lg:text-lg text-gray-700">
+                  {item.text}
+                </div>
+              </div>
+            ))}
           </div>
+        </div>
+
+        {/* Decorative SVGs */}
+        <div className="absolute inset-0">
+          <img src={svgTopRight} alt="Top Right SVG" className="block xsm:hidden absolute top-0 right-12 w-14 h-14" />
+          <img src={svgBottomLeft} alt="Bottom Left SVG" className="block xsm:hidden z-50 absolute bottom-[-200px] left-0 w-16 h-16" />
+        </div>
       </section>
     </div>
   );
-}
+};
 
 export default Home2;
