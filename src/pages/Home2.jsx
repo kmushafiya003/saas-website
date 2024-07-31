c8ab657c64354e58ff144400fbe8f0bfd42e5af5
Mushafiya <kmushafiya003@gmail.com>
2024-07-31 16:05:22 +0530

    Add divide line between counts

--- src/pages/Home2.jsx
+++ src/pages/Home2.jsx
@@ -10,13 +10,13 @@ const Home2 = () => {
   return (
     <div className="relative min-h-screen">
 
-      <Navbar2/>
+      <Navbar2 />
 
 
 
       {/* Background Image */}
       <div
-      
+
       >
         {/* Color Overlay */}
         <div className="absolute inset-0 bg-[#FFF8F2] opacity-10 z-15"></div>
@@ -44,22 +44,22 @@ const Home2 = () => {
 
       {/* Main Section */}
       <section className="relative h-screen flex flex-col items-center justify-center bg-center bg-no-repeat xsm:w-[] " style={{ backgroundImage: `url(${bgImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }} >
-         {/* Text Div */}
-         <div className="relative z-20  px-5 pt-[4%]    xsm:text-center md:px-12  md:py-12  lg:px-12 lg:py-16 max-w-screen-xl mx-auto mt-20 text-left  ">
-    {/* Heading */}
-    <h1 className="text-3xl dxs:text-3xl xxs:text-4xl xsm:text-5xl  xs:text-6xl lg:text-6xl  font-bold leading-tight text-black font-poppins  sm:mt-10 md: lg:">
-      <span className="block">A collaborative time</span>
-      <span className="block">Tracking that you Need</span>
-    </h1>
-    {/* Paragraph */}
-    <p className="text-xm mt-4 dxs:text-sm  xxs:text-base  xsm:text-lg   xs:text-xl text-black mb-6   lg:leading-relaxed max-w-lg mx-auto">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quia aliquam architecto beatae quo quibusdam.
-    </p>
-    {/* Button */}
-    <button className=" px-10 py-5 bg-custom-orange text-white font-semibold rounded-full shadow-md hover:bg-orange-600 transition-colors">
-      Start 14 Days Trial
-    </button>
-  </div>
+        {/* Text Div */}
+        <div className="relative z-20  px-5 pt-[4%]    xsm:text-center md:px-12  md:py-12  lg:px-12 lg:py-16 max-w-screen-xl mx-auto mt-20 text-left  ">
+          {/* Heading */}
+          <h1 className="text-3xl dxs:text-3xl xxs:text-4xl xsm:text-5xl  xs:text-6xl lg:text-6xl  font-bold leading-tight text-black font-poppins  sm:mt-10 md: lg:">
+            <span className="block">A collaborative time</span>
+            <span className="block">Tracking that you Need</span>
+          </h1>
+          {/* Paragraph */}
+          <p className="text-xm mt-4 dxs:text-sm  xxs:text-base  xsm:text-lg   xs:text-xl text-black mb-6   lg:leading-relaxed max-w-lg mx-auto">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quia aliquam architecto beatae quo quibusdam.
+          </p>
+          {/* Button */}
+          <button className=" px-10 py-5 bg-custom-orange text-white font-semibold rounded-full shadow-md hover:bg-orange-600 transition-colors">
+            Start 14 Days Trial
+          </button>
+        </div>
 
         {/* Fun Facts Section */}
         <div className="relative z-30 flex flex-col flex-nowrap items-center justify-center w-full mt-auto pb-12  ">
@@ -68,18 +68,18 @@ const Home2 = () => {
 
           {/* Fun Facts */}
           <div className="relative z-30 gap-2  flex flex-nowrap flex-col  xsm:flex-row mr-[10%] mt-[5%]">
-  {funFact1.map(({ id, count, text }) => (
-    <div key={id} className="bg-white border-b-2 border-gray-100 p-4 h-40  w-52 sm:p-10   md:p-10 lg:p-8  flex flex-col items-center ">
-      <div className="text-2xl  sm:text-xl md:text-2xl lg:text-3xl mt-2 font-semibold text-black mb-2">
-        {count}
-        {text === "Active user" && '+'}
-      </div>
-      <div className="text-sm sm:text-base md:text-lg lg:text-xl text-gray-700">
-        {text}
-      </div>
-    </div>
-  ))}
-</div>
+            {funFact1.map(({ id, count, text }) => (
+              <div key={id} className="bg-white border-b-2 border-gray-100 p-4 h-40  w-52 sm:p-10   md:p-10 lg:p-8  flex flex-col items-center ">
+                <div className="text-2xl  sm:text-xl md:text-2xl lg:text-3xl mt-2 font-semibold text-black mb-2">
+                  {count}
+                  {text === "Active user" && '+'}
+                </div>
+                <div className="text-sm sm:text-base md:text-lg lg:text-xl text-gray-700">
+                  {text}
+                </div>
+              </div>
+            ))}
+          </div>
 
         </div>
       </section>
--- src/pages/Home3.jsx
+++ src/pages/Home3.jsx
@@ -16,7 +16,7 @@ const Home3 = () => {
           alt="object-2"
           className="absolute block object-cover h-auto dxs:h-auto dxs:w-auto dasktopimag -top-28 xxs:hidden dxs:hidden xmd:-top-5 lg:-top-36 smd:top-0 md:hidden smd:block mmd:block "
         />
-        <div className="relative z-10 xxl:pr-64 xxl:pt-20 xmd:pr-96 xmd:pt-28 xmd:pl-52 xxl:pl-80 dxs:pt-5 dxs:pr-0 dxs:pl-0 xxs:-pt-0 xxs:pr-0 lg:pl-64 lg:pt-5 smd:pl-48 smd:pr-96 smd:pt-28 md:pl-20 just:pl-20 xs:pl-20 lg:pl-52 lg:pt-32 xlg:pl-64 xlg:pt-36 xl:pl-80 xl:pr-96 xl:pt-44 xxl:pt-10">
+        <div className="relative z-10 xxl:pr-64 xmd:pr-96 xmd:pt-28 xmd:pl-52 xxl:pl-80 dxs:pt-5 dxs:pr-0 dxs:pl-0 xxs:-pt-0 xxs:pr-0  smd:pl-48 smd:pr-96 smd:pt-28 md:pl-20 just:pl-20 xs:pl-20 lg:pl-52 lg:pt-32 xlg:pl-64 xlg:pt-36 xl:pl-80 xl:pr-96 xl:pt-44 xxl:pt-10">
           <div className="font-popfont-poppins dxs:pl-5">
             <h1 className="font-bold xxl:text-[3.5rem] dxs:text-[2rem]">
               Manage Your <br /> Business{" "}
@@ -28,7 +28,7 @@ const Home3 = () => {
               congue magna at pretium purus pretium ligula.
             </p>
           </div>
-          <div className="pt-10">
+          <div className="py-10">
             <DynamicButton
               text="Start 14 Days Trial"
               className={`w-44 font-poppins text-0.8rem h-12 leading-10 dxs:leading-0`}
@@ -40,20 +40,22 @@ const Home3 = () => {
             />
           </div>
         </div>
-       
-          <div className="relative flex justify-center gap-32  border-b-0 pt-36 lg:flex-row dxs:flex-col dxs:pt-[25rem] lg:pt-[10rem] ">
-            {
-              funFact2.map((data) => (
-                <div className="flex flex-col justify-center bg-white border-b-0 border-gray-100 dxs:border-r-0 dxs:justify-center dxs:items-center dxs:border-b-2">
-                
-                  <h1 className="text-[3rem] font-bold ">{data.count}</h1>
-              
+
+        <div className="xlg:w-8/12 lg:w-9/12  sm:w-11/12  w-10/12 mx-auto relative flex sm:flex-row flex-col gap-y-6 sm:justify-between items-center sm:divide-x-2 divide-y-2 sm:divide-y-0 divide-text-mid-grey mmd:mt-20 xl:mt-40 smd:mt-10 mt-24 top-[300px]  smd:top-0">
+          {
+            funFact2.map((data) => (
+              <div className="sm:w-[22%] xs:w-[30%] w-[40%] pt-6 pb-4  smd:px-2 xmd:py-4 ">
+
+                <div className="flex flex-col justify-center bg-white  items-center   ">
+
+                  <h1 className="lg:text-[3rem] sm:text-4xl xs:text-[3rem] text-4xl font-bold ">{data.count}</h1>
                   <p>{data.text}</p>
                 </div>
-              ))
-            }
+              </div>
+            ))
+          }
 
-          
+
         </div>
       </div>
 
